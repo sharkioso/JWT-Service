efe9bbde233075891b4b399aaebf6b347e59737b
sharkioso <tetkin04@mail.ru>
2025-07-22 02:15:55 +0500

    создан репозиторий для ролей

--- JWTService/src/main/java/com/example/JWTService/repository/UserRepository.java
+++ JWTService/src/main/java/com/example/JWTService/repository/UserRepository.java
@@ -1,11 +1,18 @@
 package com.example.JWTService.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.JWTService.entity.User;
 
+// прописал здесь стандартные методы, в дальнейшем скорее всего использованы не буду ведь проект учебный
+
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
-
+    Optional<User> findByLogin(String login);
+    Optional<User> findByEmail(String email);
+    Boolean existByLogin(String login);
+    Boolean existByEmail(String email);
 }
