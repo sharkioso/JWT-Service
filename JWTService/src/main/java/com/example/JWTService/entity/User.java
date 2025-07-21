c48df30ce2f6ad21da4cdeaff4ad6431b5feb31e
sharkioso <tetkin04@mail.ru>
2025-07-22 02:01:37 +0500

    доделан класс юзера

--- JWTService/src/main/java/com/example/JWTService/entity/User.java
+++ JWTService/src/main/java/com/example/JWTService/entity/User.java
@@ -7,6 +7,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -35,5 +38,7 @@ public class User {
     @Column(unique = true)
     private String email;
 
-    private Set<ERole> role;
+    @ManyToMany
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
 }
