70507ca889a309fff630e6b299fcc4d60f880467
sharkioso <tetkin04@mail.ru>
2025-07-22 01:49:33 +0500

    создан класс юзер, но не до конца

--- JWTService/src/main/java/com/example/JWTService/entity/User.java
+++ JWTService/src/main/java/com/example/JWTService/entity/User.java
@@ -1,5 +1,39 @@
 package com.example.JWTService.entity;
 
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@Entity
+@AllArgsConstructor
+@Table(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @NotBlank
+    @Column(unique = true)
+    private String login;
+
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    private Set<ERole> role;
 }
