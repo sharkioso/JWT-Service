a52ecb78aa098f74405b1c12a2ae7dcdb4f45522
sharkioso <tetkin04@mail.ru>
2025-07-22 02:22:31 +0500

    создан докер для поднятия базы данных, я просто невероятно верю что в этот раз все запуститься с первого раза, хоть бы так и было

--- JWTService/src/main/java/com/example/JWTService/repository/RoleRepository.java
+++ JWTService/src/main/java/com/example/JWTService/repository/RoleRepository.java
@@ -1,14 +1,11 @@
 package com.example.JWTService.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.JWTService.entity.ERole;
 import com.example.JWTService.entity.Role;
 
 @Repository
-public interface RoleRepository extends JpaRepository<Role,Long>{
-    Optional<Role> findByName(ERole name);
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
 }
--- JWTService/src/main/java/com/example/JWTService/repository/UserRepository.java
+++ JWTService/src/main/java/com/example/JWTService/repository/UserRepository.java
@@ -10,9 +10,12 @@ import com.example.JWTService.entity.User;
 // прописал здесь стандартные методы, в дальнейшем скорее всего использованы не буду ведь проект учебный
 
 @Repository
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByLogin(String login);
+
     Optional<User> findByEmail(String email);
+
     Boolean existByLogin(String login);
+
     Boolean existByEmail(String email);
 }
