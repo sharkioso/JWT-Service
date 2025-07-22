529efd5fe94c8fa118b7f34570290b218e030c5d
sharkioso <tetkin04@mail.ru>
2025-07-22 15:14:54 +0500

    сделан UserDetailsImpl

--- JWTService/src/main/java/com/example/JWTService/security/jwt/JwtUtils.java
+++ JWTService/src/main/java/com/example/JWTService/security/jwt/JwtUtils.java
@@ -1,6 +1,7 @@
 package com.example.JWTService.security.jwt;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,4 +9,16 @@ public class JwtUtils {
 
     @Value("${app.jwt.secret}")
     private String jwtSecret;
+
+    @Value("${app.jwt.expiration.ms}")
+    private int jwtExpirationMs;
+
+    @Value("${app.jwt.refresh.expiration.ms=604800000}")
+    private int jwtRefreshDuration;
+
+
+    public String generateJwtToken(Authentication authentication)
+    {
+        return "zaglushka";
+    }
 }
