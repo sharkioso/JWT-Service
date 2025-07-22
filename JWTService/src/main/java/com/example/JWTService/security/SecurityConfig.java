d711d0ee691b1219f439279386264d198289b3cf
sharkioso <tetkin04@mail.ru>
2025-07-22 16:59:40 +0500

    сделан конфиг секьюрити, пока двигаемся на нереально й вере что все запуститься

--- JWTService/src/main/java/com/example/JWTService/security/SecurityConfig.java
+++ JWTService/src/main/java/com/example/JWTService/security/SecurityConfig.java
@@ -1,18 +1,21 @@
 package com.example.JWTService.security;
 
-import java.beans.BeanProperty;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
 
 import com.example.JWTService.security.jwt.AuthEntryPoint;
+import com.example.JWTService.security.jwt.AuthTokenFilter;
 
 import lombok.RequiredArgsConstructor;
 
@@ -23,7 +26,7 @@ public class SecurityConfig {
 
     private final  AuthEntryPoint authEntryPoint;
     private final UserDetailsService userDetailsService;
-    private final JwtAuthFilter jwtAuthFilter;
+    private final AuthTokenFilter authTokenFilter;
 
 
     @Bean
@@ -43,8 +46,25 @@ public class SecurityConfig {
             );
         
             http.authenticationProvider(authenticationProvider());
-            http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+            http.addFilterBefore(authTokenFilter, UsernamePasswordAuthenticationFilter.class);
             return http.build();
 
         }
+
+
+    @Bean
+    public AuthenticationProvider authenticationProvider()
+    {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(passwordEncoder());
+
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+        
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
--- JWTService/src/main/java/com/example/JWTService/security/jwt/AuthTokenFilter.java
+++ JWTService/src/main/java/com/example/JWTService/security/jwt/AuthTokenFilter.java
@@ -6,7 +6,6 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -20,35 +19,32 @@ import lombok.RequiredArgsConstructor;
 @Component
 @RequiredArgsConstructor
 public class AuthTokenFilter extends OncePerRequestFilter {
-    
+
     private final JwtUtils jwtUtils;
     private final UserDetailsService userDetailsService;
-
-
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        try{
+        try {
             String jwt = parseJwt(request);
-            if (jwt != null && jwtUtils.validateJwtToken(jwt))
-            {
+            if (jwt != null && jwtUtils.validateJwtToken(jwt)) {
                 String username = jwtUtils.getUserNameFromJwtToken(jwt);
                 UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null,userDetails.getAuthorities());
-                
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
+
                 authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(authenticationToken);
             }
-        }catch (Exception e)
-        {}
+        } catch (Exception e) {
+        }
         filterChain.doFilter(request, response);
     }
 
-    private String parseJwt(HttpServletRequest request)
-    {
+    private String parseJwt(HttpServletRequest request) {
         String headerAuth = request.getHeader("Authorization");
-        if(headerAuth != null && headerAuth.startsWith("Bearer ")){
+        if (headerAuth != null && headerAuth.startsWith("Bearer ")) {
             return headerAuth.substring(7);
         }
         return null;
