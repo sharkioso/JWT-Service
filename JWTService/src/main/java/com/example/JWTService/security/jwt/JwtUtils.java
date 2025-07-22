4509ec0f9b1b9b94fba74d3e6c76b8b23fdffb70
sharkioso <tetkin04@mail.ru>
2025-07-22 14:26:13 +0500

    начата разработка jwtUtils

--- JWTService/src/main/java/com/example/JWTService/security/jwt/JwtUtils.java
+++ JWTService/src/main/java/com/example/JWTService/security/jwt/JwtUtils.java
@@ -1,8 +1,11 @@
 package com.example.JWTService.security.jwt;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JwtUtils {
-    
+
+    @Value("${app.jwt.secret}")
+    private String jwtSecret;
 }
